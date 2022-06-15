1fea4cdccc6b59b28bd1ce6519d352e9a3f03e04
Syerne <shubhamyerne1994@gmail.com>
2022-06-15 16:40:51 +0530

    Added: wlcome message

---
+++ LineComparisonProgram.java
@@ -0,0 +1,8 @@
+package Com.bl.Practice;
+
+public class LineComparisonProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Computation Program ");
+    }
+}
